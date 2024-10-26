8d31779e16bb351cb7074efd4004737fd104b99d
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-10-26 21:30:23 +0300

    removed the Search button - it is no longer needed

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,46 +2,14 @@ import css from './Filters.module.css';
 import Equipment from '../Equipment/Equipment';
 import Type from '../Type/Type';
 import LocationSelect from '../LocationSelect/LocationSelect';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  selectFilters,
-  selectForm,
-  selectLocation,
-} from '../../redux/filters/selectors';
-import { getAllCampers } from '../../redux/campers/operations';
-import { selectIsLoading, selectLimit } from '../../redux/campers/selectors';
-import { resetPage } from '../../redux/campers/slice';
-import { getFilters } from '../../helpers/filters';
 
 const Filters = () => {
-  const dispatch = useDispatch();
-  const location = useSelector(selectLocation);
-  const equipment = useSelector(selectFilters);
-  const form = useSelector(selectForm);
-  const limit = useSelector(selectLimit);
-  const isLoading = useSelector(selectIsLoading);
-
-  const handleClick = () => {
-    dispatch(resetPage());
-    const filters = getFilters({ page: 1, limit, location, equipment, form });
-    dispatch(getAllCampers(filters));
-  };
-
   return (
     <aside className={css.aside}>
       <LocationSelect />
       <h3 className={css.title}>Filters</h3>
       <Equipment />
       <Type />
-      <button
-        className={css.btn}
-        type="button"
-        aria-label="Search with filters"
-        onClick={handleClick}
-        disabled={isLoading}
-      >
-        Search
-      </button>
     </aside>
   );
 };

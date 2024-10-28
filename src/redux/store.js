f61b3856363535f31ba26a9b186de992d2a83455
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-10-28 22:17:19 +0200

    added persistedFavoritesReducer to store a list of favorites between page refreshes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,41 @@ import { configureStore } from '@reduxjs/toolkit';
 import { campersReducer } from './campers/slice';
 import { filtersReducer } from './filters/slice';
 import { favoritesReducer } from './favorites/slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const favoritesPersistConfig = {
+  key: 'favorites',
+  storage,
+  whitelist: ['favoritesList'],
+};
+
+const persistedFavoritesReducer = persistReducer(
+  favoritesPersistConfig,
+  favoritesReducer
+);
 
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
     filters: filtersReducer,
-    favorites: favoritesReducer,
+    favorites: persistedFavoritesReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
